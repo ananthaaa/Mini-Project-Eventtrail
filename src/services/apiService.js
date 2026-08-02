1ffd0c543222b1cf36a52f1b781111b4f335fcfa
Ananthakrishnan C A <caananthakrishnan1@gmail.com>
2026-08-02 22:32:45 +0530

    Fix bug: Incorrect API base URL environment variable name

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import * as authService from './authService';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_HTTP_API_URL;
 
 const fetchFromApi = async (endpoint, options = {}) => {
   try {
